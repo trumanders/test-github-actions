af1401b31a8e5e16868443c59ce9b86c9ab43a68
Anders Johansson <trumanders@gmail.com>
2024-11-06 10:58:48 +0100

    Remove wrong import statement

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -7,8 +7,6 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
-import org.junit.Test;
-
 
 public class CalculatorTest {
     private Calculator calculator;
