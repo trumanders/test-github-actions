72789063783ef862bad8a46e5d7f9531e88a9a8b
Anders Johansson <trumanders@gmail.com>
2024-11-12 01:26:21 +0100

    add failing test

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -22,7 +22,7 @@ public class CalculatorTest {
     @MethodSource("additionTestCasesArguments")
     @DisplayName("Addition test cases:")
     public void testAddition(double expected, double a, double b, String testCaseName) {
-        assertEquals(expected, calculator.getSumOf(a, b), testCaseName);
+        assertEquals(expected, calculator.getSumOf(a, a), testCaseName);
     }
 
     static Stream<Arguments> additionTestCasesArguments() {
