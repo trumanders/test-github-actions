6e8471e4f7732e238d6796495a07fa21f3b8195d
Anders Johansson <trumanders@gmail.com>
2024-11-12 01:24:58 +0100

    pushing comment

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
+/* PUSHING */
 
 public class CalculatorTest {
     private Calculator calculator;
